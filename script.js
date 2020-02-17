ff001e70d443000a3e1da944ef0efa72e27bce8a
Ozar <ye.koral@gmail.com>
2020-02-17 23:33:20 +0300

    recovery

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const companies= [
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
   ];
   
-  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32, 99];
   
   // for(let i = 0; i < companies.length; i++) {
   //   console.log(companies[i]);
